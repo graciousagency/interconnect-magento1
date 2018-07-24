314651f88a480fa72128b693954faa4bd947d1f8
Suraj Sanchit <s.sanchit@gracious.nl>
2018-07-25 00:21:54 +0200

    Refactored console commands to old Magento 1 style

--- shell/gracious_interconnect_sync_customer.php
+++ shell/gracious_interconnect_sync_customer.php
@@ -1,6 +1,42 @@
 <?php
 
-require_once(getcwd() . 'app/code/local/Gracious/Interconnect/Console/SyncCustomerCommand.php');
+require_once(getcwd() . '/app/code/local/Gracious/Interconnect/Console/CommandAbstract.php');
+
+/**
+ * Class Gracious_Interconnect_Console_SyncCustomerCommand
+ */
+class Gracious_Interconnect_Console_SyncCustomerCommand extends Gracious_Interconnect_Console_CommandAbstract {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run() {
+        if (!$this->config->isComplete()) {
+            $this->error(__METHOD__ . ' :: Unable to rock and roll: module config values not configured (completely) in the backend. Aborting....');
+
+            return;
+        }
+
+        $customerId = $this->getOption('id');
+        $this->evalInt($customerId);
+        /* @var $customer Mage_Customer_Model_Customer */
+        $customer = Mage::getModel('customer/customer')->load($customerId);
+
+        if ($customer === null || $customer->getId() != $customerId) {
+            $this->line('No customer found, aborting....');
+
+            return;
+        }
+
+        $this->line('Found customer, sending...');
+
+        $customerDataFactory = new Gracious_Interconnect_Http_Request_Data_Customer_Factory();
+        $requestData = $customerDataFactory->setupData($customer);
+        $this->line('Data = '.json_encode($requestData));
+        $client = new Gracious_Interconnect_Http_Request_Client();
+        $client->sendData($requestData, Gracious_Interconnect_Http_Request_Client::ENDPOINT_CUSTOMER);
+    }
+}
 
 $command = new Gracious_Interconnect_Console_SyncCustomerCommand();
 $command->run();
