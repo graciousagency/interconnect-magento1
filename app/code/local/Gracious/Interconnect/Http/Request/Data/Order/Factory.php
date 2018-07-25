d33ac9d24bafc108bcd1044f35b32122b60a7628
Suraj Sanchit <s.sanchit@gracious.nl>
2018-07-25 16:18:52 +0200

    Fix missing helper

--- app/code/local/Gracious/Interconnect/Http/Request/Data/Order/Factory.php
+++ app/code/local/Gracious/Interconnect/Http/Request/Data/Order/Factory.php
@@ -13,7 +13,7 @@ class Gracious_Interconnect_Http_Request_Data_Order_Factory extends Gracious_Int
         $quoteId = $order->getQuoteId();
         $prefixedQuoteId = $quoteId !== null ? $this->generateEntityId($quoteId, Gracious_Interconnect_Support_EntityType::QUOTE) : null;
         $orderItemFactory = new Gracious_Interconnect_Http_Request_Data_Order_Item_Factory();
-        $paymentMethod = Gracious_Interconnect_Support_Formatter::unSnakeCase($order->getPayment()->getMethod());
+        $paymentMethod = Mage::helper('interconnect/formatter')->unSnakeCase($order->getPayment()->getMethod());
         $paymentMethod = ucwords($paymentMethod);
         $total = $order->getGrandTotal();
         $discountAmount = $order->getDiscountAmount();
